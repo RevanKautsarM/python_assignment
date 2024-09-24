cb33989e9f71e212c412f5ff22c4658321b73d20
= <=>
2024-09-24 08:56:29 +0700

    kubus.py

--- kubus.py
+++ kubus.py
@@ -1,7 +1,14 @@
-s = int(input('Masukan sisi kubus: '))
+#Dibuat Oleh : Revan
+#Tanggal     : 24-09-2024
+#Soal Kubus
 
-v = s * s * s
-lp = 6 * s * s
-print('volume kubus adalah: ', v ,'cm3')
-print('==================================')
-print('luas permukaan kubus', lp ,'cm2')
+print('='*40)
+print('             PROGRAM KUBUS           ')
+print('='*40)
+
+s = int(input('Masukan Sisi Kubus: '))
+
+v = s*s*s
+lp = 6*s*s
+print('Volume Kubus Adalah: ',v ,'cm')
+print('Luas Kubus Adalah: ',lp ,'cm')
