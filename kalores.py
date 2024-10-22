3ee95ac22e37ac50fad9a119ac000d71c4356c7e
= <=>
2024-10-22 08:56:15 +0700

    (kalores.py)

---
+++ kalores.py
@@ -0,0 +1,8 @@
+print('='*40)
+print('           KALOR ES')
+print('='*40)
+
+m = float(input('Masukan Massa Es\t: '))
+l = float(input('Masukan Angka\t: '))
+q2 = m*l
+print('Kalor Lebur Es\t: ', q2)
